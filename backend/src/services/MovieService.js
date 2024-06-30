b477652039f1d3a309b61ca01dec722da9b11c62
AchilleasB <90069683+AchilleasB@users.noreply.github.com>
2024-06-30 19:57:20 +0200

    added input validation

--- backend/src/services/MovieService.js
+++ backend/src/services/MovieService.js
@@ -7,9 +7,12 @@ class MovieService {
     }
 
     async searchMovies(title, year, type, actor, director) {
-        if (!title) {
-            throw new BadRequestError('You must provide at least a title to search for movies');
-        }
+
+        // validate inputs
+        this.isValidTitle(title);
+        this.isValidYear(year);
+        this.isValidActorOrDirector(actor, 'actors');
+        this.isValidActorOrDirector(director, 'director');
 
         // fetch db objects by title
         const jsonObjects = await this.movieRepository.fetchByTitle(title);
@@ -28,21 +31,22 @@ class MovieService {
         let filteredMovies = movies;
         // filter movies by year
         if (year) {
-            filteredMovies = this.handleYearInput(movies, year)
+            filteredMovies = this.filterByYearInput(movies, year)
         }
         // filter movies by actor
         if (actor) {
-            filteredMovies = this.handleActorInput(movies, actor);
+            filteredMovies = this.filterByActorInput(movies, actor);
         }
         // filter movies by director
         if (director) {
-            filteredMovies = this.handleDirectorInput(movies, director);
+            filteredMovies = this.filterByDirectorInput(movies, director);
         }
 
         return filteredMovies;
+
     }
 
-    handleYearInput(movies, year) {
+    filterByYearInput(movies, year) {
         const filteredMoviesByYear = movies.filter(movie => movie.year === String(year));
         console.log('Filtered by year:', filteredMoviesByYear);
         if (!filteredMoviesByYear || filteredMoviesByYear.length === 0) {
@@ -51,7 +55,7 @@ class MovieService {
         return filteredMoviesByYear;
     }
 
-    handleActorInput(movies, actor) {
+    filterByActorInput(movies, actor) {
         const filteredMoviesByActor = movies.filter(movie => movie.actors.toLowerCase().includes(actor.toLowerCase()));
         console.log('Filtered by actor:', filteredMoviesByActor);
         if (!filteredMoviesByActor || filteredMoviesByActor.length === 0) {
@@ -60,13 +64,40 @@ class MovieService {
         return filteredMoviesByActor;
     }
 
-    handleDirectorInput(movies, director) {
+    filterByDirectorInput(movies, director) {
         const filteredMoviesByDirector = movies.filter(movie => movie.director.toLowerCase().includes(director.toLowerCase()));
         console.log('Filtered by director:', filteredMoviesByDirector);
         if (!filteredMoviesByDirector || filteredMoviesByDirector.length === 0) {
             throw new NotFoundError(`No movies found with director "${director}"`);
         }
         return filteredMoviesByDirector;
+    }
+
+    isValidTitle(title) {
+        if (!title) {
+            throw new BadRequestError('You must provide at least a title to search for movies.');
+        }
+        if (title.length < 3 || title.length > 30) {
+            throw new BadRequestError('Title must be between 3 and 30 characters');
+        }
+        return true;
+    }
+
+    isValidYear(year) {
+        if (year && (!/^\d{4}$/.test(year))) {
+            throw new BadRequestError('Year must be a 4 digit number');
+        }
+        return true;
+    }
+
+    isValidActorOrDirector(name, type) {
+        if (name && (name.length < 3 || name.length > 30)) {
+            throw new BadRequestError(`${type} must be between 3 and 30 characters`);
+        }
+        if (name && !/^[a-zA-Z\s]*$/.test(name)) {
+            throw new BadRequestError(`${type} must contain only letters and spaces`);
+        }
+        return true;
     }
 
 }
